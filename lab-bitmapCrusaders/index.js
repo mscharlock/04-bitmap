f3f3336e44266e589cf978a0e5d7079ca539c455
Gavin Thomas <gthomas1192@gmail.com>
2017-08-26 12:12:18 -0700

    Fixed some formatting so node.js testing would work

--- lab-bitmapCrusaders/index.js
+++ lab-bitmapCrusaders/index.js
@@ -1,15 +1,8 @@
 'use-strict';
 
-//need to be adjusted
-const Bitmap = require(`./lib/bitmap`);
-const transform = require(`./lib/transform`);
-const fs = require('fs');
+
 const read = require(`./lib/read-write`);
 
 module.exports = exports = {};
-    // let bitmap = new Bitmap(buffer);
+
 read.initFile();
-//
-// transform.invertColors(copied);
-// transform.greyscale(copied1);
-// transform.blackOut(copied2);
--- lab-bitmapCrusaders/lib/read-write.js
+++ lab-bitmapCrusaders/lib/read-write.js
@@ -1,37 +1,33 @@
 'use-strict';
 const Bitmap = require('./lib/bitmap');
-const transform = require ('./lib/transform')
+const ColorTransform = require ('./lib/transform');
 const fs = require('fs');
 
 module.exports = exports = {};
 
-exports.initFile = (path, callback) => {
-    fs.readFile(`${__dirname}/assets/palette-bitmap.bmp`, (err, data) => {
-      if(err) console.error(err);
+exports.initFile = () => {
+  fs.readFile(`${__dirname}/assets/palette-bitmap.bmp`, (err, data) => {
+    if(err) console.error(err);
 
-      let copied = new Bitmap(data);
-      let copied1 = new Bitmap(data);
-      let copied2 = new Bitmap(data);
-    });
-
+    let copied = new Bitmap(data);
+    let copied1 = new Bitmap(data);
+    let copied2 = new Bitmap(data);
     ColorTransform.invertColors(err, copied);
     ColorTransform.greyScale(err, copied1);
     ColorTransform.blackOut(err, copied2);
-  };
+  });
+};
 
-  exports.writeNew = (path, data) => {
-    return fs.writeFile(path, data, (err, data) => {
-      if(err) return err;
-      return data;
-    });
-  };
+exports.writeNew = (path, data) => {
+  return fs.writeFile(path, data, (err, data) => {
+    if(err) return err;
+    return data;
+  });
+};
 
-  exports.eraseFile = (path) => {
-    return fs.writeFile(path, '', (err, data) => {
-      if(err) return err;
-      return data;
-    });
-  };
-
-
-});
+exports.eraseFile = (path) => {
+  return fs.writeFile(path, '', (err, data) => {
+    if(err) return err;
+    return data;
+  });
+};
