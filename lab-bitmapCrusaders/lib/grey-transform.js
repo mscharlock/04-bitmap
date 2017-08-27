35f746e5aeacfb5415298ec1bc58ec43f8046493
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-27 14:16:04 -0700

    Tests and seperating transfroms

---
+++ lab-bitmapCrusaders/lib/grey-transform.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const fs = require('fs');
+const readWrite = require(`../lib/read-write`);
+console.log(readWrite);
+
+// let bmp;
+// let bmp2;
+// let bmp3;
+module.exports = exports = {};
+
+ColorTransform.prototype.greyScale = function(data) {
+  for (var i = 0; i < bmp.pixelArray.length; i += 4) {
+    bmp2 = new Bitmap(data);
+    let greyscaleArr = [bmp2.pixelArray[i], bmp2.pixelArray[i + 1], bmp2.pixelArray[i + 2], bmp2.pixelArray[i + 3]];
+    //create a new buffer off math'ed data;
+    // let newnambuffer = invertbuffer
+    console.log(greyscaleArr);
+  }
+  index.writerReader.writeNew(`${__dirname}/assets/palette-invert-bitmap.bmp`, copied1, exports.writeNew);
+};
