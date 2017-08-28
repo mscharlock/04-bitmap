0ae8428ea1c3c508238e00a5ffe724553b5dab9d
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-08-27 17:45:25 -0700

    Something isn't working in our chain of callbacks, I think??

--- lab-bitmapCrusaders/lib/transform.js
+++ lab-bitmapCrusaders/lib/transform.js
@@ -13,22 +13,32 @@ module.exports = exports = {};
 
 exports.invertColors = function(err, data) {
   console.log('this is data', data);
-  // let invertArr = data;
+  // let data = bitmap;
+  let bob = bitmap.data;
   let thing = [];
-  for (var i = 0; i < data.pixelArray.length; i += 4) {
-    let invertArr = (255-data.pixelArray[i] + 255-data.pixelArray[i+1] + 255-data.pixelArray[i+2] + 255-data.pixelArray[i+3]);
-    // parseInt(invertArr, 4);
-    console.log('this is invertarrrrr',invertArr);
-    thing.push(invertArr);
+  for (var i = 0; i < bob.length; i += 4) {
+    //do math
+    bob.pixelArray[i] = 255-bob.pixelArray[i];
+    bob.pixelArray[i+1] =255-bob.pixelArray[i+1];
+    bob.pixelArray[i+2]=255-bob.pixelArray[i+2];
+    //we don't do things on [i+3] because it's the stupid a value
+
+    // parseInt(invertArr, 2);
+    //  <-- Don't use this, this is leftover from when I was considering converting it to binary
+
+    // console.log('this is invertarrrrr',invertArr);
+    thing.push(bob);
   }
-  console.log('this is completed thing', thing);
+  // console.log('this is completed thing', thing);
 
   let rightingBuffer = Buffer.from(thing);
   // let rightingBuffer = Buffer.alloc(1087, thing);
   console.log('this is the righting buffer', rightingBuffer);
-  fs.writeFile(`./assets/palette-bitmap.bmp`, bitmap.allData);
+  fs.writeFile(`./assets/invert-bitmap.bmp`, bitmap.allData);
 
 };
+
+exports.invertColors();
 // invertArr.forEach(ele => {
 //     ele[0]*0;
 //     ele[1]*0;
