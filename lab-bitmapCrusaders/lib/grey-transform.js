817b8ec676774e9ebb911ab9761e774068683e42
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-27 21:14:11 -0700

    Ugh such a mess

--- lab-bitmapCrusaders/lib/grey-transform.js
+++ lab-bitmapCrusaders/lib/grey-transform.js
@@ -9,18 +9,18 @@ console.log(readWrite);
 // let bmp = [];
 // let bmp2 = [];
 // // let bmp3;
-module.exports = exports = {};
-
-ColorTransform.prototype.greyScale = function(data) {
-  for (var i = 0; i < bmp.pixelArray.length; i += 4) {
-    bmp2 = new Bitmap(data);
-    let greyscaleArr = [bmp2.pixelArray[i], bmp2.pixelArray[i + 1], bmp2.pixelArray[i + 2], bmp2.pixelArray[i + 3]];
-    //create a new buffer off math'ed data;
-    // let newnambuffer = invertbuffer
-    console.log(greyscaleArr);
-  }
-  index.writerReader.writeNew(`${__dirname}/assets/palette-invert-bitmap.bmp`, copied1, exports.writeNew);
-};
+// module.exports = exports = {};
+//
+// ColorTransform.prototype.greyScale = function(data) {
+//   for (var i = 0; i < bmp.pixelArray.length; i += 4) {
+//     bmp2 = new Bitmap(data);
+//     let greyscaleArr = [bmp2.pixelArray[i], bmp2.pixelArray[i + 1], bmp2.pixelArray[i + 2], bmp2.pixelArray[i + 3]];
+//     //create a new buffer off math'ed data;
+//     // let newnambuffer = invertbuffer
+//     console.log(greyscaleArr);
+//   }
+//   index.writerReader.writeNew(`${__dirname}/assets/palette-invert-bitmap.bmp`, copied1, exports.writeNew);
+// };
 
 ////// Shaun's Solution
 // module.exports = exports = function(data, bitmap, startOffset, endOffset) {
