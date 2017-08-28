59f0f4eb8e2298a3d5325ab7190cda1900d07fdf
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-27 21:03:13 -0700

    Adding my ideas

--- lab-bitmapCrusaders/lib/grey-transform.js
+++ lab-bitmapCrusaders/lib/grey-transform.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const fs = require('fs');
+const ColorTransform = (`../lib/read-write`);
 const readWrite = require(`../lib/read-write`);
+const Bitmap = require(`../lib/bitmap`);
 console.log(readWrite);
 
-// let bmp;
-// let bmp2;
+let bmp = [];
+let bmp2 = [];
 // let bmp3;
 module.exports = exports = {};
 
@@ -19,3 +21,20 @@ ColorTransform.prototype.greyScale = function(data) {
   }
   index.writerReader.writeNew(`${__dirname}/assets/palette-invert-bitmap.bmp`, copied1, exports.writeNew);
 };
+
+////// Shaun's Solution
+// module.exports = exports = function(data, bitmap, startOffset, endOffset) {
+//   for (var i = startOffset; i < endOffset; i += bitmap.bitsPerPixel / 8) {
+//     var greyScaleValueR = parseInt(data.readUInt8(i + 2), 10) * 0.3;
+//     var greyScaleValueG = parseInt(data.readUInt8(i + 1), 10) * 0.59;
+//     var greyScaleValueB = parseInt(data.readUInt8(i), 10) * 0.11;
+//
+//     var greyScale = greyScaleValueR + greyScaleValueG + greyScaleValueB;
+//
+//     data.writeUInt8(greyScale, i);
+//     data.writeUInt8(greyScale, i + 1);
+//     data.writeUInt8(greyScale, i + 2);
+//   }
+// };
+
+///
