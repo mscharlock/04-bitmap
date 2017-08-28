d5b9c76d73d05033960b27eaa98c06bae04f8b63
Shaun Wahle <shaun.wahle@gmail.com>
2017-08-27 21:25:04 -0700

    Seems like file paths are messed

--- lab-bitmapCrusaders/__test__/bitmap.test.js
+++ lab-bitmapCrusaders/__test__/bitmap.test.js
@@ -1,87 +1,76 @@
+'use strict';
 // testing
-const expect = require('chai').expect;
 const fs = require('fs');
-const handleFile = require(__dirname + '/../lib/handleFile');
-const invert = require(__dirname + '/../lib/invert');
-const greyscale = require(__dirname + '/../lib/greyscale');
+const handleFile = require(__dirname + '/../lib/read-write');
+const invert = require(__dirname + '/../lib/invert-transform');
+const greyScale = require(__dirname + '/../lib/grey-transform');
+const blueScale = require(__dirname + '/../lib/blue-transform');
+
+
 
 describe('bitmap file to a buffer to a file', () => {
   var answers = {
-    file: __dirname + '/../img/pikachu.bmp',
-    transOption: 'greyscale'
+    file: __dirname + '/../img/palette-btestmap.bmp',
+    transOption: 'greyScale',
   };
   var oldNumFiles;
   var newNumFiles;
 
-  before((done) => {
+  beforeEach((done) => {
     var oldList = fs.readdirSync(__dirname + '/../img');
     oldNumFiles = oldList.length;
     done();
   });
 
-  it('should load a bitmap into a buffer', (done) => {
+  test('should load a bitmap into a buffer', (done) => {
     handleFile.run(answers, (data) => {
-      expect(Buffer.isBuffer(data)).to.eql(true);
+      expect(Buffer.isBuffer(data)).toEqual(true);
       done();
     });
   });
 
-  it('should show that the number of files has increased', () => {
+  test('should show that the number of files has increased', () => {
     var newList = fs.readdirSync(__dirname + '/../img');
     newNumFiles = newList.length;
-    expect(newNumFiles).to.eql(oldNumFiles + 1);
+    expect(newNumFiles).toEqual(oldNumFiles + 1);
   });
 });
 
 describe('extract header data', () => {
-  it('should extract the bitmap headers into an object', () => {
-    expect(typeof handleFile.bitmapHead === 'object').to.eql(true);
+  test('should extract the bitmap headers into an object', () => {
+    expect(typeof handleFile.btestmapHead === 'object').toEqual(true);
   });
 });
 
 describe('transforms', () => {
   var testBuffer = new Buffer(4);
   var testHeaders = {};
-  testHeaders.bitsPerPixel = 32;
+  testHeaders.btestsPerPixel = 32;
 
   beforeEach(() => {
-    testBuffer.writeUInt8(50, 0);
-    testBuffer.writeUInt8(100, 1);
-    testBuffer.writeUInt8(150, 2);
+    testBuffer.wrtesteUInt8(50, 0);
+    testBuffer.wrtesteUInt8(100, 1);
+    testBuffer.wrtesteUInt8(150, 2);
   });
 
-  it('should invert the colors', () => {
+  test('should invert the colors', () => {
     invert(testBuffer, testHeaders, 0, 3);
-    expect(testBuffer.readUInt8(0)).to.eql(255 - 50);
-    expect(testBuffer.readUInt8(1)).to.eql(255 - 100);
-    expect(testBuffer.readUInt8(2)).to.eql(255 - 150);
+    expect(testBuffer.readUInt8(0)).toEqual(255 - 50);
+    expect(testBuffer.readUInt8(1)).toEqual(255 - 100);
+    expect(testBuffer.readUInt8(2)).toEqual(255 - 150);
   });
 
-  it('should redscale the colors', () => {
-    redscale(testBuffer, testHeaders, 0, 3);
-    expect(testBuffer.readUInt8(0)).to.eql(0);
-    expect(testBuffer.readUInt8(1)).to.eql(0);
-    expect(testBuffer.readUInt8(2)).to.eql(150);
+  test('should blueScale the colors', () => {
+    blueScale(testBuffer, testHeaders, 0, 3);
+    expect(testBuffer.readUInt8(0)).toEqual(50);
+    expect(testBuffer.readUInt8(1)).toEqual(0);
+    expect(testBuffer.readUInt8(2)).toEqual(0);
   });
 
-  it('should greenscale the colors', () => {
-    greenscale(testBuffer, testHeaders, 0, 3);
-    expect(testBuffer.readUInt8(0)).to.eql(0);
-    expect(testBuffer.readUInt8(1)).to.eql(100);
-    expect(testBuffer.readUInt8(2)).to.eql(0);
-  });
-
-  it('should bluescale the colors', () => {
-    bluescale(testBuffer, testHeaders, 0, 3);
-    expect(testBuffer.readUInt8(0)).to.eql(50);
-    expect(testBuffer.readUInt8(1)).to.eql(0);
-    expect(testBuffer.readUInt8(2)).to.eql(0);
-  });
-
-  it('should greyscale the colors', () => {
-    greyscale(testBuffer, testHeaders, 0, 3);
-    expect(testBuffer.readUInt8(0)).to.eql(109);
-    expect(testBuffer.readUInt8(1)).to.eql(109);
-    expect(testBuffer.readUInt8(2)).to.eql(109);
+  test('should greyScale the colors', () => {
+    greyScale(testBuffer, testHeaders, 0, 3);
+    expect(testBuffer.readUInt8(0)).toEqual(109);
+    expect(testBuffer.readUInt8(1)).toEqual(109);
+    expect(testBuffer.readUInt8(2)).toEqual(109);
   });
 });
--- lab-bitmapCrusaders/__test__/transform.test.js
+++ lab-bitmapCrusaders/__test__/transform.test.js
@@ -1,32 +1,34 @@
 'use strict';
 
 const fs = require('fs');
-const handleFile = require(__dirname + '/../lib/handleFile');
-const invert = require(__dirname + '/../lib/inverttransform');
-const greyscale = require(__dirname + '/../lib/greytransform');
+const handleFile = require(__dirname + '/../lib/read-write');
+const invert = require(__dirname + '/../lib/invert-transform');
+const greyscale = require(__dirname + '/../lib/grey-transform');
+const blueScale = require(__dirname + '/../lib/blue-transform');
+
 
 describe('bitmap file to a buffer to a file', () => {
   var answers = {
-    file: __dirname + '/../img/pikachu.bmp',
+    file: __dirname + 'palette-btestmap.bmp',
     transOption: 'greyscale',
   };
   var oldNumFiles;
   var newNumFiles;
 
-  before((done) => {
+  beforeEach((done) => {
     var oldList = fs.readdirSync(__dirname + '/../img');
     oldNumFiles = oldList.length;
     done();
   });
 
-  it('should load a bitmap into a buffer', (done) => {
+  test('should load a bitmap into a buffer', (done) => {
     handleFile.run(answers, (data) => {
       expect(Buffer.isBuffer(data)).toEqual(true);
       done();
     });
   });
 
-  it('should show that the number of files has increased', () => {
+  test('should show that the number of files has increased', () => {
     var newList = fs.readdirSync(__dirname + '/../img');
     newNumFiles = newList.length;
     expect(newNumFiles).toEqual(oldNumFiles + 1);
@@ -34,30 +36,37 @@ describe('bitmap file to a buffer to a file', () => {
 });
 
 describe('extract header data', () => {
-  it('should extract the bitmap headers into an object', () => {
-    expect(typeof handleFile.bitmapHead === 'object').toEqual(true);
+  test('should extract the bitmap headers into an object', () => {
+    expect(typeof handleFile.btestmapHead === 'object').toEqual(true);
   });
 });
 
 describe('transforms', () => {
   var testBuffer = new Buffer(4);
   var testHeaders = {};
-  testHeaders.bitsPerPixel = 32;
+  testHeaders.btestsPerPixel = 32;
 
   beforeEach(() => {
-    testBuffer.writeUInt8(50, 0);
-    testBuffer.writeUInt8(100, 1);
-    testBuffer.writeUInt8(150, 2);
+    testBuffer.wrtesteUInt8(50, 0);
+    testBuffer.wrtesteUInt8(100, 1);
+    testBuffer.wrtesteUInt8(150, 2);
   });
 
-  it('should invert the colors', () => {
+  test('should invert the colors', () => {
     invert(testBuffer, testHeaders, 0, 3);
     expect(testBuffer.readUInt8(0)).toEqual(255 - 50);
     expect(testBuffer.readUInt8(1)).toEqual(255 - 100);
     expect(testBuffer.readUInt8(2)).toEqual(255 - 150);
   });
 
-  it('should greyscale the colors', () => {
+  test('should blueScale the colors', () => {
+    blueScale(testBuffer, testHeaders, 0, 3);
+    expect(testBuffer.readUInt8(0)).toEqual(50);
+    expect(testBuffer.readUInt8(1)).toEqual(0);
+    expect(testBuffer.readUInt8(2)).toEqual(0);
+  });
+
+  test('should greyscale the colors', () => {
     greyscale(testBuffer, testHeaders, 0, 3);
     expect(testBuffer.readUInt8(0)).toEqual(109);
     expect(testBuffer.readUInt8(1)).toEqual(109);
